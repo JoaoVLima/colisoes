d9f8a7b5c9be8aee6b82b4351c18f325151da5af
JoaoVLima <joao@limadeveloper.com>
2025-10-19 21:13:43 -0300

    bucket so tem codigo estranho, to fazendo o meu

--- src/main/java/org/limadeveloper/BucketSort.java
+++ src/main/java/org/limadeveloper/BucketSort.java
@@ -1,43 +1,5 @@
 package org.limadeveloper;
 
-// list sort( s, min, max )
-//	list s;
-//	typekey min, max;
-//
-//	{
-//	int i;
-//	typekey div, maxb[M], minb[M];
-//	list head[M], t;
-//	struct rec aux;
-//	extern list Last;
-//	if (s==NULL)	return(s);
-//	if (max==min)	{
-//		for (Last=s; Last->next!=NULL; Last = Last->next);
-//		return( s );
-//		}
-//	div = (max-min) / M;	/* Find dividing factor */
-//	if (div==0)	div = 1;
-//	for (i=0; i<M; i++)	head[i] = NULL;
-//	/* Place records in buckets */
-//	while (s != NULL)	{
-//		i = (s->k-min) / div;
-//		if (i<0) i = 0;	else	if (i>=M) i = M-1;
-//		t = s;
-//		s = s->next;
-//		t->next = head[i];
-//		if (head[i]==NULL)	minb[i] = maxb[i] = t->k;
-//		head[i] = t;
-//		if ( t->k > maxb[i] )	maxb[i] = t->k;
-//		if ( t->k < minb[i] )	minb[i] = t->k;
-//		}
-//	/* sort recursively */
-//	t = &aux;
-//	for (i=0; i<M; i++)	if (head[i]!=NULL)	{
-//		t->next = sort( head[i], minb[i], maxb[i] );
-//		t = Last;
-//		}
-//	return(aux.next);
-//	}
 
 public class BucketSort {
     public BucketSort() {
@@ -46,21 +8,29 @@ public class BucketSort {
     public int[] sort(int[] arr){
         int tamanho = arr.length;
         int trocas=0;
-        for(int i=0;i<tamanho-1;i++){
-            int minI = i;
-            for(int j=i+1;j<tamanho-i-1;j++){
-                if(arr[j]<arr[minI]){
-                    minI = j;
-                }
-            }
-            if(minI!=i){
-                int temp = arr[i];
-                arr[i] = arr[minI];
-                arr[minI] = temp;
-                trocas++;
+        int numero_max_buckets = 5;
+
+        // maior numero
+        int max = arr[0];
+        for(int i=0;i<tamanho;i++){
+            if(arr[i]>max){
+                max = arr[i];
             }
         }
+
+        arr = sort(arr, max, numero_max_buckets);
+
         System.out.println(trocas);
         return arr;
+    }
+
+    private int[] sort(int[] arr, int max, int numero_max_buckets){
+        if (arr.length == 0){
+            return arr;
+        } else if (arr.length < numero_max_buckets){
+            numero_max_buckets = arr.length;
+        }
+
+
     }
 }
