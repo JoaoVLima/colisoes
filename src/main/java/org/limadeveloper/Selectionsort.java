82f2b143d3486d2cdf2a12f210a84bc7c11befb0
JoaoVLima <joao@limadeveloper.com>
2025-10-20 19:50:43 -0300

    Selectionsort

--- src/main/java/org/limadeveloper/Selectionsort.java
+++ src/main/java/org/limadeveloper/Selectionsort.java
@@ -1,24 +1,25 @@
 package org.limadeveloper;
 
 public class Selectionsort extends Sort {
-    public int[] sort(int[] arr, int tam){
+    public int[] sort(int[] arr, int tam) {
         int tamanho = arr.length;
-        int trocas=0;
-        for(int i=0;i<tamanho-1;i++){
+        for (int i = 0; i < tamanho - 1; i++) {
             int minI = i;
-            for(int j=i+1;j<tamanho-i-1;j++){
-                if(arr[j]<arr[minI]){
+            for (int j = i + 1; j < tamanho - i - 1; j++) {
+                iteracoes++;
+                if (arr[j] < arr[minI]) {
                     minI = j;
                 }
             }
-            if(minI!=i){
+            if (minI != i) {
                 int temp = arr[i];
                 arr[i] = arr[minI];
                 arr[minI] = temp;
                 trocas++;
+            }else{
+                iteracoes++;
             }
         }
-        System.out.println(trocas);
         return arr;
     }
 }
