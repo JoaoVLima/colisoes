e351c3a3666052a9c2674891d0e693a3af48a61f
JoaoVLima <joao@limadeveloper.com>
2025-10-20 19:47:11 -0300

    cocktail

--- src/main/java/org/limadeveloper/Cocktail.java
+++ src/main/java/org/limadeveloper/Cocktail.java
@@ -1,25 +1,24 @@
 package org.limadeveloper;
 
 public class Cocktail extends Sort {
-    public int[] sort(int[] arr, int tam){
+    public int[] sort(int[] arr, int tam) {
         boolean trocou = true;
-        int trocas = 0;
         int inicio = 0;
-        int fim = tam-1;
+        int fim = tam - 1;
         int temp;
-        while(trocou){
+        while (trocou) {
             trocou = false;
-            for(int i = inicio; i<fim; i++){
-                if(arr[i] > arr[i+1]){
+            for (int i = inicio; i < fim; i++) {
+                iteracoes++;
+                if (arr[i] > arr[i + 1]) {
                     temp = arr[i];
-                    arr[i] = arr[i+1];
-                    arr[i+1] = temp;
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
                     trocas++;
                     trocou = true;
                 }
             }
         }
-        System.out.println(trocas);
         return arr;
     }
 }
